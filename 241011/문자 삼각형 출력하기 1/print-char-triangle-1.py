88e1a7ce74f631118e20b32442f968cbf7e09d20
Park JongBeum <dev@parkjb.com>
2024-10-11 00:48:07 +0900

    Title: 문자 삼각형 출력하기 1, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 241011/문자 삼각형 출력하기 1/print-char-triangle-1.py
+++ 241011/문자 삼각형 출력하기 1/print-char-triangle-1.py
@@ -10,6 +10,4 @@ for i in range(N):
         alphabet %= 26
 
 for i in range(N):
-    res = '  ' * (N - 1 - i)
-    if len(res) == 0: print(*tri[i])
-    else: print(res, ' '.join(tri[i]), sep='')
+    print('  ' * (N - 1 - i), ' '.join(tri[i]), sep='')
